d7bb0483a8faba968a6dbdbf8d0f4eaa87d453b9
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-02-01 19:21:13 +0530

    5th problem second testcase added

--- VIEWS/test/server.test.js
+++ VIEWS/test/server.test.js
@@ -12,5 +12,16 @@ describe('testcase for server', function() {
             done();
         })
         });
-      
+
+        it('should print margi', function(done) {
+        const options = {
+            method : 'GET',
+            url : '/?name=margi'
+        };
+        server.inject(options,(response) => {
+            expect(response.payload).toBe(`<html><head><title>Hello margi</title></head></html>`);
+            done();
+        })
+        });
+    
     });
