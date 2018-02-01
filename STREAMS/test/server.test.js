61af5622e4f33bfb1dfbd296616dd0d8278faa80
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-02-01 22:09:07 +0530

    8th problem second testcase added

--- STREAMS/test/server.test.js
+++ STREAMS/test/server.test.js
@@ -6,8 +6,19 @@ describe('testcase for server', function() {
           url : '/'
       };
       server.inject(options,(response) => {
-          expect(response.payload).toBe('Gur Chefhvg bs Uncv-arff');
+          expect(response.statusCode).toBe(200);
           done();
       })
       });
+
+      it('rotate the data to  13 places', function(done) {
+        const options = {
+            method : 'GET',
+            url : '/'
+        };
+        server.inject(options,(response) => {
+            expect(response.payload).toBe('Gur Chefhvg bs Uncv-arff');
+            done();
+        })
+        });
     });
